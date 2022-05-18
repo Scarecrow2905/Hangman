1330c9ff9124671a6e5c7062a433afc83026357b
Tommy <tommy-sc@live.no>
2022-05-18 19:22:06 +0200

    Create Words.cs

---
+++ Hangman/Words.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Words
+    {
+        // Properties
+        public List<Words> guessWords { get; } = new List<Words>(); 
+        public string HiddenWord { get; set; }
+        public string Word;
+        public int Points { get; set; }
+
+        // Constructor
+        public Words(string aWord)
+        {
+            string word = aWord;
+            
+        }
+    }
+}
